356dff3fabb2a4d618144f8addebae5d5341996e
Pinerealm <osalami09@gmail.com>
2023-12-14 18:36:38 +0300

    Add morgan logging

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { config } from "dotenv";
+import morgan from "morgan";
+import appRouter from "./routes/index.js";
 
 config();
 const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(morgan("dev")); // Development mode
 
 export default app;

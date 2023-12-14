5eff8edfd6ac4dfabd969bfc87f015aa8231be51
Pinerealm <osalami09@gmail.com>
2023-12-14 21:12:59 +0300

    Add user controller and route for getting all users

---
+++ backend/src/controllers/user-controllers.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from "express";
+import User from "../models/User.js";
+
+export const getAllUsers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({ message: "OK", users });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "ERROR", cause: error.message });
+  }
+};
--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { getAllUsers } from "../controllers/user-controllers.js";
 
 const userRouter = Router({ strict: true });
+
+userRouter.get("/", getAllUsers);
 
 export default userRouter;

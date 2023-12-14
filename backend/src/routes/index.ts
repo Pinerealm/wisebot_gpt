8b46fe158cc37f399275558452a76cc4bbdf87a4
Pinerealm <osalami09@gmail.com>
2023-12-14 21:16:35 +0300

    Add user and chat routes to appRouter

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import userRouter from "./user-routes.js";
+import chatRouter from "./chat-routes.js";
 
 const appRouter = Router();
+
+appRouter.use("/users", userRouter);
+appRouter.use("/chats", chatRouter);
 
 export default appRouter;

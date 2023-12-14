0786f7a66a454cc6a9cb0cbf99d4a1faac139f21
Pinerealm <osalami09@gmail.com>
2023-12-14 18:33:56 +0300

    Update server console log message

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,8 @@ const PORT = process.env.EXPRESS_PORT || 5000;
 // Connect to MongoDB
 connectToDB()
   .then(() => {
-    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+    app.listen(PORT, () =>
+      console.log(`Express Server is running on port ${PORT}`)
+    );
   })
   .catch((error) => console.log(error));

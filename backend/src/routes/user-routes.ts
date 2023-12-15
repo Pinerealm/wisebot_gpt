b2fdd1c6bd414bdba99255cbe28b3e1512d6a6fc
Pinerealm <osalami09@gmail.com>
2023-12-15 16:10:37 +0300

    Add validation to user signup route

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getAllUsers, userSignup } from "../controllers/user-controllers.js";
+import { validate, signupValidator } from "../utils/validators.js";
 
 const userRouter = Router({ strict: true });
 
 userRouter.get("/", getAllUsers);
-userRouter.post("/signup", userSignup);
+userRouter.post("/signup", validate(signupValidator), userSignup);
 
 export default userRouter;

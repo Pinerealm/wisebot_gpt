bcd592f9946753c081917ab27e697051a4b884dc
Pinerealm <osalami09@gmail.com>
2023-12-15 16:26:21 +0300

    Add check for existing user during signup

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -23,11 +23,30 @@ export const userSignup = async (
 ) => {
   try {
     const { name, email, password } = req.body;
+    const existingUser = User.findOne({ email });
+    if (existingUser) {
+      return res.status(403).json({ message: "User already exists" });
+    }
     const hashedPassword = await hash(password, 10);
     const user = new User({ name, email, password: hashedPassword });
 
     await user.save();
     res.status(201).json({ message: "OK", id: user._id.toString() });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "ERROR", cause: error.message });
+  }
+};
+
+export const userLogin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    // res.status(201).json({ message: "OK", id: user._id.toString() });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "ERROR", cause: error.message });

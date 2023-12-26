49e11259d4c35d2e2a27419021a808a489819fb1
Pinerealm <osalami09@gmail.com>
2023-12-26 22:23:56 +0300

    Add logic to handle login form submission

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,8 +1,26 @@
 import { Box, Button, Typography } from "@mui/material";
 import CustomizedInput from "../components/shared/CustomizedInput";
 import { IoLogIn } from "react-icons/io5";
+import { useAuth } from "../context/AuthContext";
+import { toast } from "react-hot-toast";
 
 const Login = () => {
+  const auth = useAuth();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData);
+
+    try {
+      toast.loading("Logging in...", { id: "login" });
+      await auth?.login(data.email as string, data.password as string);
+      toast.success("Logged in successfully!", { id: "login" });
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to login!", { id: "login" });
+    }
+  };
+
   return (
     <Box width="100%" height="100%" display="flex" flex={1}>
       <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
@@ -18,6 +36,7 @@ const Login = () => {
         mt={16}
       >
         <form
+          onSubmit={handleSubmit}
           style={{
             margin: "auto",
             padding: "30px",

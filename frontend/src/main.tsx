4f6f96b20ffa5ce63c5f5a5860b3846f8141ace4
Pinerealm <osalami09@gmail.com>
2023-12-26 22:24:24 +0300

    Add Toaster component and configure axios defaults

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,6 +7,12 @@ import { ThemeProvider, createTheme } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.tsx";
 
+import { Toaster } from "react-hot-toast";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:3000/api/v1";
+axios.defaults.withCredentials = true;
+
 const theme = createTheme({
   typography: {
     fontFamily: "Roboto slab, serif",
@@ -19,6 +25,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <AuthProvider>
       <BrowserRouter>
         <ThemeProvider theme={theme}>
+          <Toaster position="top-right" />
           <App />
         </ThemeProvider>
       </BrowserRouter>

ed92714309ba1ab5f8886d15e7b2001c377c3ab7
Pinerealm <osalami09@gmail.com>
2023-12-26 22:25:07 +0300

    Add CORS middleware to allow requests from the frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,12 +2,15 @@ import express from "express";
 import { config } from "dotenv";
 import morgan from "morgan";
 import appRouter from "./routes/index.js";
+
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
 config();
 const app = express();
 
 // Middlewares
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(morgan("dev")); // Development mode

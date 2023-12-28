15410de931c1b191cf18ede75492d997b2ab7c9a
Pinerealm <osalami09@gmail.com>
2023-12-29 02:06:40 +0300

    Add user authentication endpoint

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -3,17 +3,20 @@ import {
   getAllUsers,
   userLogin,
   userSignup,
+  verifyUser,
 } from "../controllers/user-controllers.js";
 import {
   validate,
   signupValidator,
   loginValidator,
 } from "../utils/validators.js";
+import { verifyToken } from "../utils/tokens-manager.js";
 
 const userRouter = Router({ strict: true });
 
 userRouter.get("/", getAllUsers);
 userRouter.post("/signup", validate(signupValidator), userSignup);
 userRouter.post("/login", validate(loginValidator), userLogin);
+userRouter.get("/auth-status", verifyToken, verifyUser);
 
 export default userRouter;

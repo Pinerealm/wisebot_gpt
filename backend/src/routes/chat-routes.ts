a4eca8480a0281a08c0cff7a9772abfc3e2e47c5
Pinerealm <osalami09@gmail.com>
2023-12-30 00:17:15 +0300

    Add new chat completion route and middleware

--- backend/src/routes/chat-routes.ts
+++ backend/src/routes/chat-routes.ts
@@ -1,5 +1,14 @@
 import { Router } from "express";
+import { verifyToken } from "../utils/tokens-manager.js";
+import { chatCompletionValidator } from "../utils/validators.js";
+import { generateChatCompletion } from "../controllers/chat-controllers.js";
 
 const chatRouter = Router();
+chatRouter.post(
+  "/new",
+  chatCompletionValidator,
+  verifyToken,
+  generateChatCompletion
+);
 
 export default chatRouter;

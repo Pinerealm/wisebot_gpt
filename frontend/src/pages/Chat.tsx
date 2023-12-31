5078b664986247a47dc09461bec27f73768c45e6
Pinerealm <osalami09@gmail.com>
2023-12-31 19:57:10 +0300

    Refactor Chat component UI

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,7 +1,181 @@
-import React from "react";
+import { Avatar, Box, Button, IconButton, Typography } from "@mui/material";
+import { useAuth } from "../context/AuthContext";
+import { red } from "@mui/material/colors";
+import ChatItem from "../components/chat/ChatItem";
+import { IoMdSend } from "react-icons/io";
+
+const chatMessages = [
+  {
+    role: "assistant",
+    content: "Hello! How can I assist you today?",
+  },
+  {
+    role: "user",
+    content: "Hi there! I need help with setting up my email.",
+  },
+  {
+    role: "assistant",
+    content:
+      "Sure, I'd be happy to help you with that. Could you let me know which email provider you're using?",
+  },
+  {
+    role: "user",
+    content: "I use Gmail.",
+  },
+  {
+    role: "assistant",
+    content:
+      "Great! To set up your email in a mail client, you'll need to use the following settings...",
+  },
+  {
+    role: "user",
+    content: "Thanks for the information!",
+  },
+  {
+    role: "assistant",
+    content:
+      "You're welcome! If you have any more questions, feel free to ask.",
+  },
+];
 
 const Chat = () => {
-  return <div>Chat</div>;
+  const auth = useAuth();
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        mt: 3,
+        gap: 3,
+      }}
+    >
+      <Box
+        sx={{
+          display: { md: "flex", xs: "none", sm: "none" },
+          flex: 0.2,
+          flexDirection: "column",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            width: "100%",
+            height: "60vh",
+            bgcolor: "rgb(17,29,39)",
+            borderRadius: 5,
+            flexDirection: "column",
+            mx: 3,
+          }}
+        >
+          <Avatar
+            sx={{
+              mx: "auto",
+              my: 2,
+              bgcolor: "white",
+              color: "black",
+              fontWeight: 700,
+            }}
+          >
+            {auth?.user?.name[0]}
+            {auth?.user?.name.split(" ")[1][0]}
+          </Avatar>
+          <Typography sx={{ mx: "auto", fontFamily: "work sans", my: 4, p: 3 }}>
+            You are logged in as {auth?.user?.name}
+            <br />
+            Feel free to ask any question.
+            <br />
+            Avoid sharing personal information.
+          </Typography>
+          <Button
+            sx={{
+              width: "200px",
+              m: "auto",
+              color: "white",
+              fontWeight: 700,
+              borderRadius: 3,
+              bgcolor: red[300],
+              ":hover": {
+                bgcolor: red.A400,
+              },
+            }}
+          >
+            Clear conversation
+          </Button>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flex: { md: 0.8, xs: 1, sm: 1 },
+          flexDirection: "column",
+          px: 3,
+        }}
+      >
+        <Typography
+          sx={{
+            textAlign: "center",
+            fontSize: "40px",
+            fontWeight: 600,
+            color: "white",
+            mb: 2,
+            mx: "auto",
+          }}
+        >
+          Model - GPT 3.5 Turbo
+        </Typography>
+        <Box
+          sx={{
+            width: "100%",
+            height: "60vh",
+            borderRadius: 3,
+            mx: "auto",
+            display: "flex",
+            flexDirection: "column",
+            overflow: "scroll",
+            overflowX: "hidden",
+            overflowY: "auto",
+            scrollBehavior: "smooth",
+          }}
+        >
+          {chatMessages.map((chat, index) => (
+            <ChatItem
+              content={chat.content}
+              role={chat.role as "user" | "assistant"}
+              key={index}
+            />
+          ))}
+        </Box>
+        <div
+          style={{
+            width: "100%",
+            padding: "20px",
+            borderRadius: 8,
+            backgroundColor: "rgb(17,29,39)",
+            display: "flex",
+            margin: "auto",
+          }}
+        >
+          <input
+            type="text"
+            style={{
+              width: "100%",
+              backgroundColor: "transparent",
+              padding: "10px",
+              border: "none",
+              outline: "none",
+              color: "white",
+              fontSize: "20px",
+            }}
+          />
+          <IconButton sx={{ ml: "auto", color: "white" }}>
+            <IoMdSend />
+          </IconButton>
+        </div>
+      </Box>
+    </Box>
+  );
 };
 
 export default Chat;

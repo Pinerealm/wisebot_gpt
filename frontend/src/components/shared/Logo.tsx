7219c195e9b07b9498fc619f456a14a0c342c391
Pinerealm <osalami09@gmail.com>
2024-01-04 17:10:38 +0300

    Update Logo styling and font size

--- frontend/src/components/shared/Logo.tsx
+++ frontend/src/components/shared/Logo.tsx
@@ -1,17 +1,17 @@
-import Typography from "@mui/material/Typography";
-import { Link } from "react-router-dom";
+import Typography from '@mui/material/Typography';
+import { Link } from 'react-router-dom';
 
 const Logo = () => {
   return (
     <div
       style={{
-        display: "flex",
-        marginRight: "auto",
-        alignItems: "center",
-        gap: "10px",
+        display: 'flex',
+        marginRight: 'auto',
+        alignItems: 'center',
+        gap: '10px',
       }}
     >
-      <Link to={"/"}>
+      <Link to={'/'}>
         <img
           src="robot-icon.png"
           alt="robot-icon"
@@ -22,13 +22,13 @@ const Logo = () => {
       </Link>
       <Typography
         sx={{
-          display: { md: "block", sm: "none", xs: "none" },
-          mr: "auto",
-          fontWeight: "800",
-          textShadow: "2px 2px 20px #000",
+          display: { md: 'block', sm: 'none', xs: 'none' },
+          mr: 'auto',
+          fontWeight: '800',
+          textShadow: '2px 2px 20px #000',
         }}
       >
-        <span style={{ fontSize: "20px" }}>WISEBOT</span>
+        <span style={{ fontSize: '40px' }}>WiseBOT</span>
       </Typography>
     </div>
   );

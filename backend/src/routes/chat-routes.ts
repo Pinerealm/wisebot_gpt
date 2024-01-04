26b798e067319811f2887ec26a83be235ffd1d7d
Pinerealm <osalami09@gmail.com>
2024-01-04 17:15:03 +0300

    Add endpoint to send all current user chats

--- backend/src/routes/chat-routes.ts
+++ backend/src/routes/chat-routes.ts
@@ -1,14 +1,19 @@
-import { Router } from "express";
-import { verifyToken } from "../utils/tokens-manager.js";
-import { chatCompletionValidator, validate } from "../utils/validators.js";
-import { generateChatCompletion } from "../controllers/chat-controllers.js";
+import { Router } from 'express';
+import { verifyToken } from '../utils/tokens-manager.js';
+import { chatCompletionValidator, validate } from '../utils/validators.js';
+import {
+  generateChatCompletion,
+  sendChatsToUser,
+} from '../controllers/chat-controllers.js';
 
 const chatRouter = Router();
 chatRouter.post(
-  "/new",
+  '/new',
   validate(chatCompletionValidator),
   verifyToken,
   generateChatCompletion
 );
+
+chatRouter.get('/all-chats', verifyToken, sendChatsToUser);
 
 export default chatRouter;

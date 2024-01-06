7af56d80f1fcdaf8c2f30392d7950f647f855518
Pinerealm <osalami09@gmail.com>
2024-01-06 15:07:44 +0300

    Add deleteChats function to chat-controllers

--- backend/src/controllers/chat-controllers.ts
+++ backend/src/controllers/chat-controllers.ts
@@ -55,3 +55,26 @@ export const sendChatsToUser = async (
     res.status(500).json({ message: 'ERROR', cause: error.message });
   }
 };
+
+export const deleteChats = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await User.findById(res.locals.jwtData.id);
+    if (!user)
+      return res.status(401).send('User not registered OR token expired!');
+    if (user._id.toString() !== res.locals.jwtData.id) {
+      return res.status(401).send('Incorrect token!');
+    }
+
+    //@ts-ignore
+    user.chats = [];
+    await user.save();
+    res.status(200).json({ message: 'OK' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'ERROR', cause: error.message });
+  }
+};

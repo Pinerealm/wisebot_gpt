bb58077d36dd97d141556119c21efde7582f38c0
Pinerealm <osalami09@gmail.com>
2024-01-06 17:03:01 +0300

    Add logout endpoint

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,22 +1,24 @@
-import { Router } from "express";
+import { Router } from 'express';
 import {
   getAllUsers,
   userLogin,
+  userLogout,
   userSignup,
   verifyUser,
-} from "../controllers/user-controllers.js";
+} from '../controllers/user-controllers.js';
 import {
   validate,
   signupValidator,
   loginValidator,
-} from "../utils/validators.js";
-import { verifyToken } from "../utils/tokens-manager.js";
+} from '../utils/validators.js';
+import { verifyToken } from '../utils/tokens-manager.js';
 
 const userRouter = Router({ strict: true });
 
-userRouter.get("/", getAllUsers);
-userRouter.post("/signup", validate(signupValidator), userSignup);
-userRouter.post("/login", validate(loginValidator), userLogin);
-userRouter.get("/auth-status", verifyToken, verifyUser);
+userRouter.get('/', getAllUsers);
+userRouter.post('/signup', validate(signupValidator), userSignup);
+userRouter.post('/login', validate(loginValidator), userLogin);
+userRouter.get('/auth-status', verifyToken, verifyUser);
+userRouter.get('/logout', verifyToken, userLogout);
 
 export default userRouter;

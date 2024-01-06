cdf37fc268906cb7fca2059f1252f07f04932436
Pinerealm <osalami09@gmail.com>
2024-01-06 17:02:23 +0300

    Add userLogout function

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,8 +1,8 @@
-import { NextFunction, Request, Response } from "express";
-import User from "../models/User.js";
-import { compare, hash } from "bcrypt";
-import { generateToken } from "../utils/tokens-manager.js";
-import { COOKIE_NAME, COOKIE_OPTIONS } from "../utils/constants.js";
+import { NextFunction, Request, Response } from 'express';
+import User from '../models/User.js';
+import { compare, hash } from 'bcrypt';
+import { generateToken } from '../utils/tokens-manager.js';
+import { COOKIE_NAME, COOKIE_OPTIONS } from '../utils/constants.js';
 
 export const getAllUsers = async (
   req: Request,
@@ -11,10 +11,10 @@ export const getAllUsers = async (
 ) => {
   try {
     const users = await User.find();
-    res.status(200).json({ message: "OK", users });
+    res.status(200).json({ message: 'OK', users });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "ERROR", cause: error.message });
+    res.status(500).json({ message: 'ERROR', cause: error.message });
   }
 };
 
@@ -27,7 +27,7 @@ export const userSignup = async (
     const { name, email, password } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(403).json({ message: "User already exists" });
+      return res.status(403).json({ message: 'User already exists' });
     }
 
     const hashedPassword = await hash(password, 10);
@@ -36,19 +36,19 @@ export const userSignup = async (
 
     // Clear cookie before setting it
     res.clearCookie(COOKIE_NAME, {
-      path: "/",
+      path: '/',
       ...COOKIE_OPTIONS,
     });
 
-    const token = generateToken(user._id.toString(), user.email, "14d");
+    const token = generateToken(user._id.toString(), user.email, '14d');
     res.cookie(COOKIE_NAME, token, {
       ...COOKIE_OPTIONS,
       maxAge: 14 * 24 * 60 * 60 * 1000,
     });
-    res.status(201).json({ message: "OK", name: user.name, email: user.email });
+    res.status(201).json({ message: 'OK', name: user.name, email: user.email });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "ERROR", cause: error.message });
+    res.status(500).json({ message: 'ERROR', cause: error.message });
   }
 };
 
@@ -60,30 +60,30 @@ export const userLogin = async (
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    if (!user) return res.status(403).json({ message: "User not registered" });
+    if (!user) return res.status(403).json({ message: 'User not registered' });
 
     const isPasswordCorrect = await compare(password, user.password);
     if (!isPasswordCorrect) {
-      console.log("Invalid credentials");
-      return res.status(403).json({ message: "Invalid credentials" });
+      console.log('Invalid credentials');
+      return res.status(403).json({ message: 'Invalid credentials' });
     }
 
     // Clear cookie before setting it
     res.clearCookie(COOKIE_NAME, {
-      path: "/",
+      path: '/',
       ...COOKIE_OPTIONS,
     });
 
-    const token = generateToken(user._id.toString(), user.email, "14d");
+    const token = generateToken(user._id.toString(), user.email, '14d');
     res.cookie(COOKIE_NAME, token, {
       ...COOKIE_OPTIONS,
       maxAge: 14 * 24 * 60 * 60 * 1000,
     });
 
-    res.status(200).json({ message: "OK", name: user.name, email: user.email });
+    res.status(200).json({ message: 'OK', name: user.name, email: user.email });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "ERROR", cause: error.message });
+    res.status(500).json({ message: 'ERROR', cause: error.message });
   }
 };
 
@@ -95,14 +95,39 @@ export const verifyUser = async (
   try {
     const user = await User.findById(res.locals.jwtData.id);
     if (!user)
-      return res.status(401).send("User not registered OR token expired!");
+      return res.status(401).send('User not registered OR token expired!');
     if (user._id.toString() !== res.locals.jwtData.id) {
-      return res.status(401).send("Incorrect token!");
+      return res.status(401).send('Incorrect token!');
     }
 
-    res.status(200).json({ message: "OK", name: user.name, email: user.email });
+    res.status(200).json({ message: 'OK', name: user.name, email: user.email });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "ERROR", cause: error.message });
+    res.status(500).json({ message: 'ERROR', cause: error.message });
+  }
+};
+
+export const userLogout = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await User.findById(res.locals.jwtData.id);
+    if (!user)
+      return res.status(401).send('User not registered OR token expired!');
+    if (user._id.toString() !== res.locals.jwtData.id) {
+      return res.status(401).send('Incorrect token!');
+    }
+
+    res.clearCookie(COOKIE_NAME, {
+      path: '/',
+      ...COOKIE_OPTIONS,
+    });
+
+    res.status(200).json({ message: 'OK', name: user.name, email: user.email });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'ERROR', cause: error.message });
   }
 };

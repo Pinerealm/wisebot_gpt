e92a7640e6846384af25dfb48902d48ca5847b61
Pinerealm <osalami09@gmail.com>
2024-01-11 14:40:12 +0300

    Update Footer component styling

--- frontend/src/components/shared/Footer.tsx
+++ frontend/src/components/shared/Footer.tsx
@@ -6,16 +6,16 @@ const Footer = () => {
       <div
         style={{
           width: '100%',
-          padding: 20,
           minHeight: '20vh',
           maxHeight: '30vh',
-          marginTop: 50,
+          marginTop: 60,
         }}
       >
         <p
           style={{
             fontSize: '30px',
             textAlign: 'center',
+            padding: '20px',
           }}
         >
           ALX Specialisation Final Project

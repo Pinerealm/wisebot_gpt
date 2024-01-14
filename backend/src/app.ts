e24114e07492e28fc9e5ac2959b08926914e2937
Pinerealm <osalami09@gmail.com>
2024-01-14 23:47:51 +0300

    Change hardcoded CORS origin string to a constant

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,23 @@
-import express from "express";
-import { config } from "dotenv";
-import morgan from "morgan";
-import appRouter from "./routes/index.js";
+import express from 'express';
+import { config } from 'dotenv';
+import morgan from 'morgan';
+import appRouter from './routes/index-routes.js';
 
-import cors from "cors";
-import cookieParser from "cookie-parser";
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { ORIGIN } from './utils/constants.js';
 
 config();
+// Express application instance.
 const app = express();
 
 // Middlewares
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: ORIGIN, credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(morgan("dev")); // Development mode
+app.use(morgan('dev')); // Development mode
 
-app.use("/api/v1", appRouter);
+// API routes
+app.use('/api/v1', appRouter);
 
 export default app;
